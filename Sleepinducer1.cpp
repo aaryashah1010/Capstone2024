f1c49373de273c5e88d6bda596b63cb76e253b92
aaryashah1010 <aaryashah1010@gmail.com>
2024-03-05 22:59:58 +0530

    aarya

--- Sleepinducer1.cpp
+++ Sleepinducer1.cpp
@@ -1,7 +1,46 @@
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int main()
-{
-    cout<<"aarya";
+
+class inmates {
+public:
+    string name;
+    int ear_id;
+    vector<int> sleep_time;
+    int minutes;
+
+    // Constructor to initialize name, ear_id, minutes, and sleep_time vector
+    inmates(string name, int ear_id, int minutes, const vector<int>& sleep_time) : name(name), ear_id(ear_id), minutes(minutes), sleep_time(sleep_time) {
+    }
+};
+
+class dorm {
+public:
+    string dorm_name;
+    int channels_id;
+    int music_id;
+};
+
+// Function to display inmate's data
+void displayInmates(const inmates& inmate) {
+    cout << "Name: " << inmate.name << endl;
+    cout << "Ear ID: " << inmate.ear_id << endl;
+    cout << "Minutes: " << inmate.minutes << endl;
+    cout << "Sleep Time: ";
+    for(int i = 0; i < 7; i++) {
+        cout << inmate.sleep_time[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    
+    vector<int> sleep_time = {8, 7, 8, 9, 7, 6, 8};
+    
+    inmates i1("Aarya", 202301036, 10, sleep_time); 
+    displayInmates(i1);
+
     return 0;
 }
---
+++ practice.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+using namespace std;
+int main()
+{
+    vector <int>v(5);
+    for(int i=0;i<5;i++)
+    {
+        cin>>v[i];
+    }
+    ofstream fout;
+    fout.open("zoom.txt");
+    for(int i=0;i<5;i++)
+    {
+        fout<<v[i];
+    }
+    fout.close();
+
+}
